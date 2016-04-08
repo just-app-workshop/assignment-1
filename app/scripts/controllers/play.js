89f079463139cb3fb6a9145a0cbf24bd39e6c407
Stavros Kounis <skounis@gmail.com>
2016-04-08 22:39:58 +0300

    feat(play) Demonstrate the sortBy for a collection

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -10,9 +10,34 @@
 angular.module('assignment1App')
   .controller('PlayCtrl', function () {
     var numbers = [ 2, 5, 23, 14, 11, 66, 3, 32, 77, 0];
+
+    var collection = [
+      {
+        name: 'Steve',
+        score: 4
+      }, {
+        name: 'Nick',
+        score: 10
+      }, {
+        name: 'Kim',
+        score: 9
+      }, {
+        name: 'Evan',
+        score: 6
+      }, {
+        name: 'John',
+        score: 7
+      }, {
+        name: 'Jim',
+        score: 5
+      }
+    ]
     var oddNumbers = odd(numbers);
     var evenNumbers = even(numbers);
     var sortedNumbers = sort(numbers);
+
+    var sortedCollectionByScore = sortCollectionByScore(collection);
+    var sortedCollectionByName = sortCollectionByName(collection);
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -24,6 +49,10 @@ angular.module('assignment1App')
     this.oddNumbers = oddNumbers;
     this.evenNumbers = evenNumbers;
     this.sortedNumbers = sortedNumbers;
+
+    this.collection = collection;
+    this.sortedCollectionByScore = sortedCollectionByScore;
+    this.sortedCollectionByName = sortedCollectionByName;
 
     function odd(arr){
       return _.filter(arr, function(element){
@@ -40,6 +69,18 @@ angular.module('assignment1App')
     function sort(arr){
       return _.sortBy(arr, function(num){
         return num; }
+      );
+    }
+
+    function sortCollectionByScore(collection){
+      return _.sortBy(collection, function(element){
+        return element.score; }
+      );
+    }
+
+    function sortCollectionByName(collection){
+      return _.sortBy(collection, function(element){
+        return element.name; }
       );
     }
 
